aa28c0cb40de44869a44ab826195e4e0b95c281b
phatthana <phatthana@me.com>
2019-06-16 15:37:50 +0800

    use workbox instead

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,28 +1,31 @@
-const cacheName = 'myPWACache';
-const filesToCache = [
-    '/',
-    '/index.html'
-];
+// const cacheName = 'myPWACache';
+// const filesToCache = [
+//     '/',
+//     '/index.html',
+//     '/static/css/2.b179ccdc.chunk.css'
+// ];
 
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            cache.addAll(filesToCache);
-        })
-    )
-});
+// self.addEventListener("install", (e) => {
+//     e.waitUntil(
+//         caches.open(cacheName).then((cache) => {
+//             cache.addAll(filesToCache);
+//         })
+//     )
+// });
 
-self.addEventListener("activate", (e) => {
-    e.waitUntil(
-        console.log("Activated Service worker")
-    )
- })
+// self.addEventListener("activate", (e) => {
+//     e.waitUntil(
+//         console.log("Activated Service worker")
+//     )
+//  })
 
- self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request)
-        .then((response) => {
-            return response || fetch(e.request)
-        })
-    )
- })
+//  self.addEventListener("fetch", (e) => {
+//     e.respondWith(
+//         caches.match(e.request)
+//         .then((response) => {
+//             return response || fetch(e.request)
+//         })
+//     )
+//  })
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
